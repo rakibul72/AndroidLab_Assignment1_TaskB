429af8f2b48e72b7f5b09dba2853d8c076ae6667
unknown <rakibulislam.bd98@gmail.com>
2020-03-14 21:06:28 +0600

    This is the 1st commit of the project.

---
+++ app/src/main/java/com/example/assignment1_taskb/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.assignment1_taskb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editText1,editText2;
+    private Button addButton,subButton;
+    private TextView resultTextView;
+
+    String[] countryNames;
+    private Spinner spinner;
+    private TextView spinnerTextViewId;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText1=(EditText) findViewById(R.id.editText1Id);
+        editText2=(EditText) findViewById(R.id.editText2Id);
+
+        addButton=(Button) findViewById(R.id.addButtonId);
+        subButton=(Button) findViewById(R.id.subButtonId);
+
+        resultTextView=(TextView) findViewById(R.id.resultTextViewId);
+
+        addButton.setOnClickListener(this);
+        subButton.setOnClickListener(this);
+
+        countryNames=getResources().getStringArray(R.array.Country);
+
+        spinner=(Spinner) findViewById(R.id.spinnerId);
+        spinnerTextViewId=(TextView) findViewById(R.id.spinnerTextViewId);
+
+        ArrayAdapter<String> adapter =new ArrayAdapter<String>(this,R.layout.sample_view,R.id.textviewsampleId,countryNames);
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        try{
+            String number1=editText1.getText().toString();
+            String number2=editText2.getText().toString();
+
+            double num1=Double.parseDouble(number1);
+            double num2=Double.parseDouble(number2);
+
+            if(v.getId()==R.id.addButtonId)
+            {
+                double sum=num1+num2;
+                resultTextView.setText("Result :"+sum);
+            }
+
+            if(v.getId()==R.id.subButtonId)
+            {
+                double sub=num1-num2;
+                resultTextView.setText("Result :"+sub);
+            }
+        }catch(Exception e)
+        {
+            Toast.makeText(MainActivity.this,"Please enter numbers",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
